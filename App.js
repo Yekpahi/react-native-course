85404bb9895ab48779f7a7d72db981fdc34c8d61
Prince Yekpahi <princeyekpahi@hotmail.fr>
2021-11-02 16:40:20 +0100

    scrollView and list

--- App.js
+++ App.js
@@ -1,65 +1,78 @@
-import React, {useState} from 'react';
-import {Text, View, StyleSheet, Button, Linking} from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState("Alice")
-    const [bol, setBol] = useState(false)
- const onClickHandler = () => {
-   setName('Abu Ben Riu')
-   setBol(true)
- }
+
+  const [Items, setItems] = useState([
+    { key: 1, item: 'Item 1' },
+    { key: 2, item: 'Item 2' },
+    { key: 3, item: 'Item 3' },
+    { key: 4, item: 'Item 4' },
+    { key: 5, item: 'Item 5' },
+    { key: 6, item: 'Item 6' },
+    { key: 7, item: 'Item 7' },
+    { key: 8, item: 'Item 8' },
+    { key: 44, item: 'Item 9' },
+    { key: 68, item: 'Item 27' },
+    { key: 0, item: 'Item 78' },
+  ]);
+  const [Refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, { key: 69, item: 'Item 69' }]);
+    setRefreshing(false);
+  }
+
   return (
-   <View style = {styles.body}>
-  <View style = {styles.v1}>
-   <Text>1</Text>
-   </View>
-   <View style = {styles.v2}>
-    <Text>2</Text>
-   </View>
-   <View style = {styles.v3}>
-   <Text>3</Text>
-   </View>
-   </View>
+    <ScrollView
+
+      style={styles.body}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+          colors={['#ff00ff']}
+        />
+      }
+    >
+      {
+        Items.map((object) => {
+          return (
+            <View style={styles.item} key={object.key}>
+              <Text style={styles.text}>{object.item}</Text>
+            </View>
+          )
+        })
+      }
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection : 'column',
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    backgroundColor: '#f9c2ff',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
   },
-
-  v1: {
-    flex: 1,
-    height: 50,
-    
-    width:50,
-    backgroundColor: 'red',
+  item: {
+    margin: 10,
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  
-  v2: {
-    flex: 1,
-    height: 50,
-    width:50,
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center',
+  text: {
+    color: '#000000',
+    fontSize: 45,
+    fontStyle: 'italic',
+    margin: 10,
   },
-  
-  v3: {
-    flex: 1,
-    height: 50,
-    width:50,
-    backgroundColor: 'yellow',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
 });
+
 export default App;
