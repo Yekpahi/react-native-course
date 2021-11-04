1530773e27c0a7c7c7bb421491a0ed1ce5035b4a
Prince Yekpahi <princeyekpahi@hotmail.fr>
2021-11-04 11:59:35 +0100

    toucheble pressable button

--- App.js
+++ App.js
@@ -1,27 +1,62 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
 /* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
+
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-} from 'react-native';
+import {StyleSheet, View, Text, Button, TextInput, Pressable} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('');
+  const [name, SetName] = useState('');
+  const [name2, SetName2] = useState('');
+  const [submitted, SetSubmitted] = useState(false);
+  const [submitted2, SetSubmitted2] = useState(false);
+  const onpr = () => {
+    SetSubmitted(!submitted);
+  };
 
+  const onpro = () => {
+    SetSubmitted2(!submitted2);
+  };
   return (
     <View style={styles.body}>
-      <Text style ={styles.text}> Votre nom est {name} </Text>
+      {submitted ? (
+        <Text style={styles.text}> Votre nom est {name} </Text>
+      ) : null}
+
       <TextInput
-       style={styles.input}
-       placeholder="useless placeholder"
-       editable = {true}
-       maxLength={5}
-       onChangeText = {(value) => setName(value)}
-       secureTextEntry
-       />
+        style={styles.input}
+        placeholder="e.g. John"
+        onChangeText={value => SetName(value)}
+      />
+      <View style={styles.button}>
+        <Button title={submitted ? 'Clear' : 'Submit'} onPress={onpr} />
+      </View>
+
+
+      <TextInput
+        style={styles.input}
+        placeholder="e.g. John"
+        onChangeText={value => SetName2(value)}
+      />
+      <Pressable
+          onPress={onpro}
+          hitSlop={{ top: 10, bottom: 10, right: 30, left: 10 }}
+          android_ripple={{color:'#00f'}}
+          style={({ pressed }) => [
+            { backgroundColor: pressed ? '#dddddd' : '#00ff00' },
+            styles.button,
+          ]}>
+              <Text style={styles.text}>
+          {submitted2 ? 'Clear' : 'Submit'}
+        </Text>
+          </Pressable>
+          {submitted2 ?
+        <Text style={styles.text}>
+          You are registered as {name2}
+        </Text>
+        :
+        null
+      }
     </View>
   );
 };
@@ -37,17 +72,21 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    borderRadius:30,
-    paddingLeft:30,
-    textAlign:'center',
+    borderRadius: 30,
+    paddingLeft: 30,
+    textAlign: 'center',
   },
-  text : {
+  text: {
     fontSize: 20,
     margin: 10,
-    position:'relative',
+    position: 'relative',
     top: 15,
-    left:'30%',
-
+    left: '30%',
+  },
+  button: {
+    width: 150,
+    height: 50,
+    alignItems: 'center',
   },
 });
 
