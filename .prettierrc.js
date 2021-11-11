5caaf17baa1a25fb6c3346b597e37935dbb19116
Prince Yekpahi <princeyekpahi@hotmail.fr>
2021-11-12 00:02:47 +0100

    first commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,12 +3,4 @@ module.exports = {
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
-  arrowParens: 'avoid',
-  "extends": ["prettier"],
-  "plugins": ["prettier"],
-  "rules": {
-    "prettier/prettier": "error",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off"
-  }
 };
--- src/App.js
+++ src/App.js
@@ -1,182 +1,99 @@
-import React, { useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Pressable,
-  Alert,
-  ToastAndroid,
-  Modal,
-  Image,
-  ImageBackground,
-} from 'react-native';
-import MashButton from './CustomButton';
-import Header from './Header';
 
-const App = () => {
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToDo from './screens/ToDo';
+import Done from './screens/Done';
+import Task from './screens/Task';
+import Splash from './screens/Splash';
+import Map from './screens/Map';
+import Camera from './screens/Camera';
+import { Provider } from 'react-redux';
+import { Store } from './redux/store';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-  const [name, SetName] = useState('');
-  const [submitted, SetSubmitted] = useState(false);
-  const [showWarning, SetshowWarning] = useState(false);
-  const onPressHandler = () => {
-    if (name.length > 3) {
-      SetSubmitted(!submitted);
-    } else {
-      SetshowWarning(true);
-    }
-  }
+const Tab = createBottomTabNavigator();
 
+function HomeTabs() {
   return (
-    <ImageBackground
-      style={styles.body}
-      source={{ uri: 'https://cdn.pixabay.com/photo/2013/07/12/12/35/texture-145968_960_720.png' }}
-    >
-      <Header />
-      <Modal
-        visible={showWarning}
-        transparent
-        onRequestClose={() =>
-          SetshowWarning(false)
-        }
-        animationType='slide'
-        hardwareAccelerated
-      >
-        <View style={styles.centered_view}>
-          <View style={styles.warning_modal}>
-            <View style={styles.warning_title}>
-              <Text style={styles.text}>WARNING!</Text>
-            </View>
-            <View style={styles.warning_body}>
-              <Text style={styles.text}>The name must be longer than 3 charachters</Text>
-            </View>
-            <Pressable
-              onPress={() => SetshowWarning(false)}
-              style={styles.warning_button}
-              android_ripple={{ color: '#fff' }}
-            >
-              <Text style={styles.text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}>
-        Please write your name:
-      </Text>
-      <TextInput
-        style={styles.input}
-        placeholder='e.g. John'
-        onChangeText={(value) => SetName(value)}
-      />
-      <MashButton
-        onPressFunction={onPressHandler}
-        title={submitted ? 'Clear' : 'Submit'}
-        color={'#00ff00'}
-      />
-      <MashButton
-        onPressFunction={() => { }}
-        title={'Test'}
-        color={'#ff00ff'}
-        style={{ margin: 10 }}
-      />
-      {/* <Pressable
-        onPress={onPressHandler}
-        hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
-        android_ripple={{ color: '#00f' }}
-        style={({ pressed }) => [
-          { backgroundColor: pressed ? '#dddddd' : '#00ff00' },
-          styles.button
-        ]}
-      >
-        <Text style={styles.text}>
-          {submitted ? 'Clear' : 'Submit'}
-        </Text>
-      </Pressable> */}
-      {
-        submitted ?
-          <View style={styles.body}>
-            <Text style={styles.text}>
-              You are registered as {name}
-            </Text>
-            <Image
-              style={styles.image}
-              source={require('../assets/done.png')}
-              resizeMode='stretch'
-            />
-          </View>
-          :
-          <Image
-            style={styles.image}
-            source={{ uri: 'https://cdn.pixabay.com/photo/2018/01/04/15/51/404-error-3060993_960_720.png' }}
-            resizeMode='stretch'
-          />
+    <Tab.Navigator
+      screenOptions={
+        ({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => {
+            let iconName;
+            if (route.name === 'To-Do') {
+              iconName = 'clipboard-list';
+              size = focused ? 25 : 20;
+            } else if (route.name === 'Done') {
+              iconName = 'clipboard-check';
+              size = focused ? 25 : 20;
+            }
+            return (
+              <FontAwesome5
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        })
       }
-    </ImageBackground >
+      tabBarOptions={{
+        activeTintColor: '#0080ff',
+        inactiveTintColor: '#777777',
+        labelStyle: { fontSize: 15, fontWeight: 'bold' }
+      }}
+    >
+      <Tab.Screen name={'To-Do'} component={ToDo} />
+      <Tab.Screen name={'Done'} component={Done} />
+    </Tab.Navigator>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  text: {
-    color: '#000000',
-    fontSize: 20,
-    margin: 10,
-    textAlign: 'center',
-  },
-  input: {
-    width: 200,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  button: {
-    width: 150,
-    height: 50,
-    alignItems: 'center',
-  },
-  centered_view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00000099'
-  },
-  warning_modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: '#ffffff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 20,
-  },
-  warning_title: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff0',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-  },
-  warning_body: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  warning_button: {
-    backgroundColor: '#00ffff',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    margin: 10,
-  }
-});
+const RootStack = createStackNavigator();
+
+function App() {
+  return (
+    <Provider store={Store}>
+      <NavigationContainer>
+        <RootStack.Navigator
+          initialRouteName="Splash"
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#0080ff'
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontSize: 25,
+              fontWeight: 'bold'
+            }
+          }}
+        >
+          <RootStack.Screen
+            name="Splash"
+            component={Splash}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <RootStack.Screen
+            name="My Tasks"
+            component={HomeTabs}
+          />
+          <RootStack.Screen
+            name="Task"
+            component={Task}
+          />
+          <RootStack.Screen
+            name="Camera"
+            component={Camera}
+          />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
 
 export default App;
